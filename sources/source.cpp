933c7c9fc94f9f1e4075c4c7349626e7c599cee5
Dmitry Kalinin <mitya17092001@gmail.com>
2020-10-17 15:06:41 +0300

    edited copyright comments

--- sources/source.cpp
+++ sources/source.cpp
@@ -1,3 +1,3 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Dmitry Kalinin <x.shreddered.x@gmail.com
 
 #include <header.hpp>
--- tests/test.cpp
+++ tests/test.cpp
@@ -1,7 +1,8 @@
-// Copyright 2018 Your Name <your_email>
+// Copyright 2020 Dmitry Kalinin <x.shreddered.x@gmail.com
 
 #include <gtest/gtest.h>
 
 TEST(Example, EmptyTest) {
+
     EXPECT_TRUE(true);
 }
